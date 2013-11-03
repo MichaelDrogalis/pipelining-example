94257f16b32e62edf9e5a1108d7266702daf6b27
Michael Drogalis <mjd3089@rit.edu>
2013-11-03 13:27:06 -0500

    Initial commit.

---
+++ src/pipelining_example/core.clj
@@ -0,0 +1,51 @@
+(ns pipelining-example.core
+  (:require [clojure.core.async :refer [chan go >! <!]]))
+
+(defn a [x]
+  (println "[A]:" x)
+  x)
+
+(defn b [x]
+  (println "[B]:" x)
+  x)
+
+(defn c [x]
+  (println "[C]:" x)
+  x)
+
+;;; ((comp c b a) 42)
+
+(def printer (agent nil))
+
+(defn m [x]
+  (send printer (fn [_] (println "[A]:" x)))
+  (Thread/sleep 500)
+  x)
+
+(defn n [x]
+  (send printer (fn [_] (println "[B]:" x)))
+  (Thread/sleep 1000)
+  x)
+
+(defn o [x]
+  (send printer (fn [_] (println "[C]:" x)))
+  (Thread/sleep 2000)
+  x)
+
+;;; ((comp o n m) 42)
+
+(defn pipeline []
+  (let [bound 10000
+        m-ch (chan bound)
+        n-ch (chan bound)
+        o-ch (chan bound)]
+    (go (while true (>! n-ch (m (<! m-ch)))))
+    (go (while true (>! o-ch (n (<! n-ch)))))
+    (go (while true (o (<! o-ch))))
+    m-ch))
+
+(def head-ch (pipeline))
+
+(doseq [k (range 10)]
+  (go (>! head-ch k)))
+
